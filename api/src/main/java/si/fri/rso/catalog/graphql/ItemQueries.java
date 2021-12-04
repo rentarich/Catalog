62c5dfda6e76fadaab1a1d1dd710adf5a018700e
Jer Pelhan <jp4861@student.uni-lj.si>
2021-12-04 16:22:57 +0100

    Graphql.

---
+++ api/src/main/java/si/fri/rso/catalog/graphql/ItemQueries.java
@@ -0,0 +1,83 @@
+package si.fri.rso.catalog.graphql;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Name;
+import org.eclipse.microprofile.graphql.Query;
+import si.fri.rso.catalog.models.dtos.Item;
+import si.fri.rso.catalog.services.beans.ItemBean;
+import com.kumuluz.ee.graphql.annotations.GraphQLClass;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+@GraphQLApi
+@ApplicationScoped
+public class ItemQueries {
+
+
+    @Inject
+    private ItemBean itemBean;
+
+
+    @Name("helloWorld")
+    public String hello() {
+        return "Hello world!";
+    }
+
+    @GraphQLQuery
+    public List<Item> items() {
+
+        return itemBean.getItems();
+    }
+
+    @GraphQLQuery
+    public Item getItem(@GraphQLArgument(name = "id") Integer id) {
+        return itemBean.getItem(id);
+    }
+
+
+    @GraphQLMutation
+    public Item addItem (@GraphQLArgument(name = "item") Item item) {
+        return itemBean.createItem(item);
+    }
+
+    @GraphQLMutation
+    public  boolean deleteItem(@GraphQLArgument(name = "id") Integer id) {
+        return itemBean.deleteItem(id);
+    }
+}
+
+
+/*
+    HOW TO QUERY?
+
+    POST localhost:5555/graphql
+        {
+        items{
+          id
+          title
+          category
+          description
+        }
+    POST localhost:5555/graphql
+        mutation deleteItem{
+            deleteItem(id:1)
+        }
+
+    POST localhost:5555/graphql
+           mutation addItem{
+            addItem(item:{title:"screw", description:"dd", category:"work"}) {
+                description
+                category
+            }
+}
+
+
+
+
+}
+
+
+ */

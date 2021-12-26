7ea6ebf90cc53eb8b970d53b199e9a58a37f5814
Jer Pelhan <jp4861@student.uni-lj.si>
2021-12-26 11:23:39 +0100

    health

--- api/src/main/java/si/fri/rso/catalog/api/v1/health/ItemsHealthCheck.java
+++ api/src/main/java/si/fri/rso/catalog/api/v1/health/ItemsHealthCheck.java
@@ -1,29 +1,29 @@
-//package si.fri.rso.catalog.api.v1.health;
-//import org.eclipse.microprofile.health.HealthCheck;
-//import org.eclipse.microprofile.health.HealthCheckResponse;
-//import org.eclipse.microprofile.health.Liveness;
-//import si.fri.rso.catalog.models.dtos.Item;
-//import si.fri.rso.catalog.services.beans.ItemBean;
-//
-//import javax.enterprise.context.ApplicationScoped;
-//import javax.inject.Inject;
-//import java.util.List;
-//
-//@Liveness
-//@ApplicationScoped
-//public class ItemsHealthCheck implements HealthCheck {
-//    @Inject
-//    private ItemBean itemBean;
-//
-//    @Override
-//    public HealthCheckResponse call() {
-//        try {
-//            itemBean.getItems();
-//        } catch (Exception e){
-//            return HealthCheckResponse.down(ItemsHealthCheck.class.getSimpleName());
-//        }
-//
-//        return HealthCheckResponse.up(ItemsHealthCheck.class.getSimpleName());
-//
-//    }
-//}
+package si.fri.rso.catalog.api.v1.health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+import si.fri.rso.catalog.models.dtos.Item;
+import si.fri.rso.catalog.services.beans.ItemBean;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+@Liveness
+@ApplicationScoped
+public class ItemsHealthCheck implements HealthCheck {
+    @Inject
+    private ItemBean itemBean;
+
+    @Override
+    public HealthCheckResponse call() {
+        try {
+            itemBean.getItems();
+        } catch (Exception e){
+            return HealthCheckResponse.down(ItemsHealthCheck.class.getSimpleName());
+        }
+
+        return HealthCheckResponse.up(ItemsHealthCheck.class.getSimpleName());
+
+    }
+}
